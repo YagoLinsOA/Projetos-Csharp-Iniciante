f1b0fe32bae22af78b67c71db34b5d3c1e88f82a
Yago Lins <yago.lins@hotmail.com>
2021-07-13 20:40:35 -0300

    Create Program.cs

---
+++ Uri teste Cálculo Salario com bonus/Uri teste Cálculo Salario com bonus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+namespace Uri_teste_Cálculo_Salario_com_bonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salario, vendas, total;
+
+            nome = Console.ReadLine();
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            total = salario + (vendas * 0.15);
+
+            Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
