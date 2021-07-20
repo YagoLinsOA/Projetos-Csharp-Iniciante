f1345666f1d15a2fafe764022670daaea955b81f
Yago Lins <yago.lins@hotmail.com>
2021-07-20 19:26:11 -0300

    atualização-projeto

---
+++ CalculoVolumeEsfera.Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+namespace Uri_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R, volume;
+            double pi = 3.14159;
+
+            R = double.Parse(Console.ReadLine());
+
+            volume = (4.0 / 3) * pi * Math.Pow(R, 3);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
