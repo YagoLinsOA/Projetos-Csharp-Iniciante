f16cadc67c3eb355ef699dd81d98f7f0e3f54dca
Yago Lins <yago.lins@hotmail.com>
2021-07-21 17:58:12 -0300

    atualização-projetos

---
+++ CalculoArea.Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri_Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, At, Ac,Atrpz, Aq, Ar;
+            double Pi = 3.14159;
+
+            string[] vet = Console.ReadLine().Split(' ');
+             A = double.Parse(vet[0], CultureInfo.InvariantCulture);
+             B = double.Parse(vet[1], CultureInfo.InvariantCulture);
+             C = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            At = A * C / 2;
+
+            Ac = Pi * Math.Pow(C, 2);
+
+            Atrpz = (A + B) * C / 2;
+
+            Aq = Math.Pow(B, 2);
+
+            Ar = A * B;
+
+            Console.WriteLine("TRIANGULO: " + At.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + Ac.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + Atrpz.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + Aq.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + Ar.ToString("F3", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
---
+++ ModDaDivisao.Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uri_Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, anos, meses, dias, resto;
+
+            idade = int.Parse(Console.ReadLine());
+
+            anos = idade / 365;
+            resto = idade % 365;
+            meses = resto / 30;
+            dias = resto % 30;
+
+            Console.WriteLine(anos + " ano(s)");
+            Console.WriteLine(meses + " mes(es)");
+            Console.WriteLine(dias + " dia(s)");
+            
+
+        }
+    }
+}
