50b78e48e5b8f4f067e6223ee929d51fd174d932
Yago Lins <yago.lins@hotmail.com>
2021-08-12 17:48:20 -0300

    atualização-projetos

---
+++ ProjetoComVetor.Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int[] vet;
+            
+            N = int.Parse(Console.ReadLine());
+            vet = new int[N];
+
+            string[] aux = Console.ReadLine().Split(' ');
+
+            for (int i=0; i<N; i++)
+            {
+                vet[i] = int.Parse(aux[i]);
+            }
+            for (int i=0; i<N; i++)
+            {
+                if(vet[i] % 2 == 0)
+                {
+                    Console.Write(vet[i] + " ");
+                }
+            }
+            Console.WriteLine();
+
+            int cont = 0;
+            for (int i=0; i<N; i++)
+            {
+                if ( vet[i] % 2 == 0)
+                {
+                    cont++;
+                }
+            }
+            Console.WriteLine(cont);
+
+        }
+    }
+}
---
+++ ValidaçãoSenha.Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("Acesso Permitido");
+
+        }
+    }
+}
