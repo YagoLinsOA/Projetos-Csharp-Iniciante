aaeab2a62de21c16c45ac2393f1cd7ae4c80f172
Yago Lins <yago.lins@hotmail.com>
2021-08-17 19:05:22 -0300

    atualização-projetos

---
+++ ProjetoMatriz.Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int[,] Mat;
+            N = int.Parse(Console.ReadLine());
+
+            Mat = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    Mat[i, j] = int.Parse(s[j]);
+
+                }
+            }
+            for (int i = 0; i < N; i++)
+            {
+                int soma = 0;
+                for (int j = 0; j < N; j++)
+                {
+                    soma = soma + Mat[i, j];
+                }
+                Console.WriteLine(soma);
+
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Projetos Csharp Iniciante
- Projetos Csharp para iniciantes realizados na plataforma Udemy.
-Aqui estão alguns pequenos códigos iniciantes de um curso em C#, usando a plataforma de questões URI Online Judge. 
+# Projetos C# Iniciante
+
+ Projetos C# para iniciantes realizados na plataforma Udemy.
+
+- Aqui estão alguns pequenos códigos iniciantes de um curso em C#, usando a plataforma de questões URI Online Judge. 
+  - Programas que abordam Cálculos de Área; Gasto médio de Combustível; Aumento de Salário; Cálculo Área de Terreno; 
+  - Programas com Estruturas Sequenciais; Estruturas Condicionais; Estrutura Repetitivas; Vetores; Matrizes.
+
